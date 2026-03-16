29c13cf819c555d27a2e0b62598e2e549059b2f1
tejasr952 <2320040005@klh.edu.in>
2026-03-16 09:27:21 +0530

    Time: 544 ms (5.04%), Space: 48.7 MB (99.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,49 +1,52 @@
 import java.util.*;
 
 class Solution {
+
     public List<List<String>> groupAnagrams(String[] strs) {
-        
-        List<List<String>> result = new ArrayList<>();
-        
-        // sort array based on sorted string
-        Arrays.sort(strs, (a, b) -> {
-            char[] c1 = a.toCharArray();
-            char[] c2 = b.toCharArray();
-            
-            Arrays.sort(c1);
-            Arrays.sort(c2);
-            
-            return new String(c1).compareTo(new String(c2));
-        });
-        
-        
-        List<String> temp = new ArrayList<>();
-        temp.add(strs[0]);
-        
-        for (int i = 1; i < strs.length; i++) {
-            
-            if (isAnagram(strs[i], strs[i-1])) {
-                temp.add(strs[i]);
-            } else {
-                result.add(new ArrayList<>(temp));
-                temp.clear();
-                temp.add(strs[i]);
+
+        List<List<String>> ans = new ArrayList<>();
+
+        boolean[] used = new boolean[strs.length];
+
+        for (int i = 0; i < strs.length; i++) {
+
+            if (used[i]) continue;
+
+            List<String> group = new ArrayList<>();
+            group.add(strs[i]);
+            used[i] = true;
+
+            for (int j = i + 1; j < strs.length; j++) {
+
+                if (!used[j] && isAnagram(strs[i], strs[j])) {
+                    group.add(strs[j]);
+                    used[j] = true;
+                }
             }
+
+            ans.add(group);
         }
-        
-        result.add(temp);
-        
-        return result;
+
+        return ans;
     }
-    
-    
+
+
+    // check anagram without sorting
     public boolean isAnagram(String a, String b) {
-        char[] c1 = a.toCharArray();
-        char[] c2 = b.toCharArray();
-        
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        
-        return Arrays.equals(c1, c2);
+
+        if (a.length() != b.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            count[a.charAt(i) - 'a']++;
+            count[b.charAt(i) - 'a']--;
+        }
+
+        for (int x : count) {
+            if (x != 0) return false;
+        }
+
+        return true;
     }
 }
