014f27e2ae25316e7b53056a26c5b13dff57945d
tejasr952 <2320040005@klh.edu.in>
2026-03-16 09:19:31 +0530

    Time: 50 ms (5.04%), Space: 49.4 MB (95.22%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,15 +1,49 @@
+import java.util.*;
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map = new HashMap<>();
-        for(String word:strs){
-            char[] arr = word.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
-            if(!map.containsKey(key)){
-                map.put(key,new ArrayList<>());
+        
+        List<List<String>> result = new ArrayList<>();
+        
+        // sort array based on sorted string
+        Arrays.sort(strs, (a, b) -> {
+            char[] c1 = a.toCharArray();
+            char[] c2 = b.toCharArray();
+            
+            Arrays.sort(c1);
+            Arrays.sort(c2);
+            
+            return new String(c1).compareTo(new String(c2));
+        });
+        
+        
+        List<String> temp = new ArrayList<>();
+        temp.add(strs[0]);
+        
+        for (int i = 1; i < strs.length; i++) {
+            
+            if (isAnagram(strs[i], strs[i-1])) {
+                temp.add(strs[i]);
+            } else {
+                result.add(new ArrayList<>(temp));
+                temp.clear();
+                temp.add(strs[i]);
             }
-            map.get(key).add(word);
         }
-        return new ArrayList<>(map.values());
+        
+        result.add(temp);
+        
+        return result;
+    }
+    
+    
+    public boolean isAnagram(String a, String b) {
+        char[] c1 = a.toCharArray();
+        char[] c2 = b.toCharArray();
+        
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        
+        return Arrays.equals(c1, c2);
     }
 }
